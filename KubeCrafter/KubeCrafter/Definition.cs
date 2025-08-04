8967184b9646b6f42221ae0b2b8dd8c466e287a4
Cyberft <adi.stein@outlook.cz>
2025-08-04 21:44:12 +0200

    Add defi.

---
+++ KubeCrafter/KubeCrafter/Definition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KubeCrafter.Core
+{
+    public class Definition
+    {
+        public string Type { get; set; }
+        public string[] Fields { get; set; }
+        public Dictionary<string, List<string>> Output { get; set; }
+
+
+
+
+    }
+}
