1484d39d2eea567de65e8748d8a86c93aaa52919
KikiZC <kikizc22@gmail.com>
2025-08-05 09:24:59 +0200

    Adds dynamic formatting feature
    Introduces dynamic formatting capabilities by allowing flexible and customizable output based on defined templates.
    
    Adds components for handling text lines and repeatable blocks with dynamic variables.
    
    Implements a converter to deserialize formatted data into specific line types.

---
+++ KubeCrafter/KubeCrafter/Dynamic Fromating/DynamicFormater.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace KubeCrafter.Core
+{
+    public static class DynamicFormater
+    {
+        public static string FormateSetting(object format)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            JsonElement jsonValue = JsonSerializer.Deserialize(format);
+
+
+
+
+
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ KubeCrafter/KubeCrafter/Dynamic Fromating/OutputLineConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace KubeCrafter.Core.Dynamic_Fromating
+{
+    public class OutputLineConverter : JsonConverter<IOutputLine>
+    {
+        public override IOutputLine Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.String)
+            {
+                return new TextLine { Value = reader.GetString() };
+            }
+
+            using var doc = JsonDocument.ParseValue(ref reader);
+            var obj = doc.RootElement;
+
+            if (obj.TryGetProperty("repeat", out var repeat))
+            {
+                var block = new RepeatBlock
+                {
+                    Type = repeat.GetProperty("type").GetString(),
+                    Var = repeat.GetProperty("var").GetString(),
+                    Format = repeat.GetProperty("format").GetString(),
+                };
+
+                if (repeat.TryGetProperty("key", out var keyProp))
+                    block.Key = keyProp.GetString();
+
+                return block;
+            }
+
+            throw new JsonException("Neznámý typ výstupního řádku.");
+        }
+
+        public override void Write(Utf8JsonWriter writer, IOutputLine value, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException("Nepodporováno – pouze čtení.");
+        }
+    }
+}
