c11d3e30083b7fa8976f571005c3dbe96b1b60de
KikiZC <kikizc22@gmail.com>
2025-08-09 20:03:19 +0200

    Configures setting templates
    Adds setting templates for different data types.
    Configures a template selector to choose templates based on setting type.
    Includes a project reference to the Core project.

---
+++ KubeCrafter/KubeCrafter.WinApp/Templates/SettingTemplateSelector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using KubeCrafter.Core.Base.Setting;
+
+namespace KubeCrafter.WinApp.Templates
+{
+    public class SettingTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate TextTemplate { get; set; }
+        public DataTemplate NumberTemplate { get; set; }
+        public DataTemplate ListTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            Setting setting = (Setting)item;
+
+            switch (setting.Type.TypeEnum)
+            {
+                case SettingTypeEnum.Text:
+                    return TextTemplate;
+                case SettingTypeEnum.Number:
+                    return NumberTemplate;
+                case SettingTypeEnum.Array:
+                    return ListTemplate;
+                default:
+                    return base.SelectTemplate(item, container);
+            }
+        }
+    }
+}
